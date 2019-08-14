ce9b6494f1acb9177d7a39f8a0684459915ed63c
Kamil <kamil.tomala@ath.pl>
2019-08-14 13:57:10 +0200

    Delete Pet component from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import Pet from './components/Pet';
 
 const App = () => {
   return React.createElement("div", {}, [
