5c1b2728d9feb990e0443ec5c0f03e1ebc008fda
Kamil <kamil.tomala@ath.pl>
2019-08-16 13:39:02 +0200

    Animal selection

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
     const [location, updateLocation] = useState('Seattle, WA');
+    const [animal, updateAnimal] = useState("");
+
     return (
         <div className="search-params">
             <form>
@@ -13,6 +16,22 @@ const SearchParams = () => {
                         placeholder="Location" 
                         onChange={e => updateLocation(e.target.value)}
                     />
+                    <label htmlFor="animal">
+                        Animal
+                        <select
+                            id="animal"
+                            value={animal}
+                            onChange={e => updateAnimal(e.target.value)}
+                            onBlur={e => updateAnimal(e.target.value)}
+                        >
+                            <option />
+                            {ANIMALS.map(animal => (
+                                <option key={animal} value={animal}>
+                                    {animal}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
                 </label>
             </form>
         </div>
