787ae1454ed496869478581853b3d4c710023690
Kamil <kamil.tomala@ath.pl>
2019-08-16 13:54:19 +0200

    Add breed to SearchParams

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from '@frontendmasters/pet';
 const SearchParams = () => {
     const [location, updateLocation] = useState('Seattle, WA');
     const [animal, updateAnimal] = useState("");
+    const [breed, updateBreed] = useState("");
+    const [breeds, updateBreeds] = useState([]);
 
     return (
         <div className="search-params">
@@ -30,6 +32,24 @@ const SearchParams = () => {
                                     {animal}
                                 </option>
                             ))}
+                        </select>
+                    </label>
+                    <label htmlFor="breed">
+                        Breed
+                        <select
+                            disabled={!breeds.length}
+                            id="breed"
+                            value={breed}
+                            onChange={e => updateBreed(e.target.value)}
+                            onBlur={e => updateBreed(e.target.value)}
+                        >
+                            <option />
+                            {breeds.map(breed => (
+                                <option key={breed} value={breed}>
+                                    {breed}
+                                </option>
+                            ))}
+
                         </select>
                     </label>
                 </label>
